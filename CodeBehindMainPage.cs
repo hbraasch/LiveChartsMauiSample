e6a9092a0b51d7aec30d9d14b6afb167b3592e8c
Heinrich Braasch <hbraasch@users.noreply.github.com>
2022-05-20 08:54:41 +1000

    Tabbed paged to demonstrate

--- CodeBehindMainPage.cs
+++ CodeBehindMainPage.cs
@@ -9,22 +9,29 @@ using System.Threading.Tasks;
 
 namespace MauiSample
 {
-    internal class CodeBehindMainPage : ContentPage
+    public class CodeBehindMainPage : ContentPage
     {
+        CartesianChart chart;
         public CodeBehindMainPage()
         {
-            var chart = new CartesianChart();
+            var label = new Label { Text = "Entry - I'm displaying" };
+            chart = new CartesianChart() { VerticalOptions = LayoutOptions.Center };
             chart.Series = new ISeries[]
             {
                 new LineSeries<double>
                 {
-                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6, 6, 6 },
                     Fill = null
                 }
             };
-            var grid = new Grid();
-            grid.Add(chart);
+            var grid = new Grid() { Margin = 10};
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.Add(label,0,0);
+            grid.Add(chart, 0, 1);
             Content = grid;
         }
+
     }
 }
---
+++ StartupPageTabbed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiSample
+{
+    internal class StartupPageTabbed : TabbedPage
+    {
+        public StartupPageTabbed()
+        {
+            Children.Add(new AppShell() { Title = "Shell"});
+            Children.Add(new CodeBehindMainPage() { Title = "NoShell" });
+        }
+    }
+}
