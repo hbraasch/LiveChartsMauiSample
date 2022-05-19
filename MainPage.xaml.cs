6475da416114fb4117f1093a80c342a8855f2225
Heinrich Braasch <hbraasch@users.noreply.github.com>
2022-05-19 12:52:57 +1000

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiSample;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
---
+++ ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace MauiSample
+{
+    public class ViewModel
+    {
+        public ISeries[] Series { get; set; }
+            = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null
+                }
+            };
+    }
+}
